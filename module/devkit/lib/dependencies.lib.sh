9e9697d46c561a44ffe460d33e46abc3f9ff782f
Jesús López Alonso <jeslopalo@sandbox.es>
2018-05-15 13:56:05 +0200

    Add new 'devkit' command to manage configuration

---
+++ module/devkit/lib/dependencies.lib.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+check_for_dependencies() {
+#    hash curl 2>/dev/null || { echo >&2 "error: I require curl but it's not installed.  Aborting."; exit 1; }
+#    hash jq 2>/dev/null || { echo >&2 "error: I require jq but it's not installed.  Aborting."; exit 1; }
+    return
+}

74344c09d2507a7b9e91a77478e38e105508d5f4
Jesús López Alonso <jeslopalo@sandbox.es>
2018-06-07 00:36:05 +0200

    Improve color library

--- lib/color.lib.sh
+++ lib/color.lib.sh
@@ -1,32 +1,20 @@
 #!/usr/bin/env bash
 
-declare tput=$(which tput)
-declare -i terminal=$(test -t 1)
-declare -i with_colors
+_tput=":"
+# Use colors, but only if connected to a terminal, and that terminal supports them.
+if [[ ${DEVKIT_COLORS:-0} = 1 ]] && test -t 1; then
+    _tput=$(which tput)
+    _number_of_colors=$($_tput colors)
 
-color::tput() {
-    local -r args="${@:-sgr0}"
-    local -i ncolors=0
-
-    # Use colors, but only if connected to a terminal, and that terminal supports them.
-    if [[ -z ${with_colors:-} ]]; then
-        if [[ -n "$tput" ]]; then
-            ncolors=$(tput colors)
-        fi
-
-        [[ $terminal ]] && [[ -n ${ncolors:-} ]] && [[ $ncolors -ge 8 ]] && with_colors=1 || with_colors=0
-    fi
-
-    if [[ $with_colors = 1 ]]; then $tput ${args[@]}; fi
-}
+    [[ ${_number_of_colors:-0} -ge 8 ]] || _tput=":"
+fi
 
 color::test() {
-    local -ri ncolors=$(color::tput colors)
-
+    local -i ncolors=${_number_of_colors:-0}
     printf "Printing %d colors...\\n" "$ncolors"
 
     for((c=0; c < $ncolors; c++)); do
-        printf "%s %-3d $reset%s %-3d $reset" "$(color::regular $c)" "$c" "$(color::background $c)" "$c"
+        printf "%s %-3d $reset%s %-3d $reset" "$($_tput setaf $c)" "$c" "$($_tput setab $c)" "$c"
 
         if (( c == 7 )); then
             printf "$reset\\n"
@@ -39,45 +27,66 @@ color::test() {
     printf "$reset\\n"
 }
 
-color::test2() {
-    local -r type="${1:-regular}"
-    local -ri ncolors=$(color::tput colors)
-
-    printf "Printing %d colors...\\n" "$ncolors"
-    for((c=0; c < $ncolors; c++)); do
-        printf "%s %-3d $reset" "$(color::$type $c)" "$c"
-
-        if (( c == 7 )); then
-            printf "$reset\\n"
-        fi
-
-        if (( c == 15 )) || (( c > 15 )) && (( (c-15) % 6 == 0 )); then
-            printf "$reset\\n"
-        fi
-    done
-    printf "$reset\\n"
-}
-
-color::regular() { color::tput setaf "${1:-7}"; }
-color::background() { color::tput setab "${1:-7}"; }
-color::reset() { color::tput sgr0; }
-color::bold() { color::tput bold; }
-color::underline() { color::tput smul; }
-color::nounderline() { color::tput rmul; }
-color::reverse() { color::tput rev; }
-
-color::println() {
-    local -r color="${1:-}"
-    shift
-    local -r message="$@"
-    printf "${color}${message}${reset}"
-}
-
-strip_color_codes() {
-    perl -pe 's/\e\[?.*?[\@-~]//g' $content
-}
+color::regular() { $_tput setaf "${1:-7}"; }
+color::background() { $_tput setab "${1:-7}"; }
+color::reset() { $_tput sgr0; }
+color::bold() { $_tput bold; }
+color::underline() { $_tput smul; }
+color::nounderline() { $_tput rmul; }
+color::reverse() { $_tput rev; }
 
 ## COLORS
+
+#Regular
+black=$($_tput setaf 0)
+red=$($_tput setaf 1)
+green=$($_tput setaf 2)
+yellow=$($_tput setaf 3)
+blue=$($_tput setaf 4)
+purple=$($_tput setaf 5)
+cyan=$($_tput setaf 6)
+white=$($_tput setaf 7)
+
+#Bright
+bblack=$($_tput setaf 8)
+bred=$($_tput setaf 9)
+bgreen=$($_tput setaf 10)
+byellow=$($_tput setaf 11)
+bblue=$($_tput setaf 12)
+bpurple=$($_tput setaf 13)
+bcyan=$($_tput setaf 14)
+bwhite=$($_tput setaf 15)
+
+#Background
+bg_black=$($_tput setab 0)
+bg_red=$($_tput setab 1)
+bg_green=$($_tput setab 2)
+bg_yellow=$($_tput setab 3)
+bg_blue=$($_tput setab 4)
+bg_purple=$($_tput setab 5)
+bg_cyan=$($_tput setab 6)
+bg_white=$($_tput setab 7)
+
+#Brightackground
+bg_bblack=$($_tput setab 8)
+bg_bred=$($_tput setab 9)
+bg_bgreen=$($_tput setab 10)
+bg_byellow=$($_tput setab 11)
+bg_bblue=$($_tput setab 12)
+bg_bpurple=$($_tput setab 13)
+bg_bcyan=$($_tput setab 14)
+bg_bwhite=$($_tput setab 15)
+
+reset=$($_tput sgr0)
+bold=$($_tput bold)
+underline=$($_tput smul)
+remove_underline=$($_tput rmul)
+reverse=$($_tput rev)
+
+error_color="$bwhite$bold"
+warn_color="$white$bold"
+
+color::println() { printf "${1:-}${@:2}${reset}"; }
 
 black()  { color::println $black  "$@"; }
 grey()   { color::println $bblack "$@"; }
@@ -89,52 +98,8 @@ purple() { color::println $purple "$@"; }
 cyan()   { color::println $cyan   "$@"; }
 white()  { color::println $white  "$@"; }
 
-#Regular
-black="$(color::regular 0)"
-red="$(color::regular 1)"
-green="$(color::regular 2)"
-yellow="$(color::regular 3)"
-blue="$(color::regular 4)"
-purple="$(color::regular 5)"
-cyan="$(color::regular 6)"
-white="$(color::regular 7)"
+# utils
 
-#Bright
-bblack="$(color::regular 8)"
-bred="$(color::regular 9)"
-bgreen="$(color::regular 10)"
-byellow="$(color::regular 11)"
-bblue="$(color::regular 12)"
-bpurple="$(color::regular 13)"
-bcyan="$(color::regular 14)"
-bwhite="$(color::regular 15)"
-
-#Background
-bg_black="$(color::background 0)"
-bg_red="$(color::background 1)"
-bg_green="$(color::background 2)"
-bg_yellow="$(color::background 3)"
-bg_blue="$(color::background 4)"
-bg_purple="$(color::background 5)"
-bg_cyan="$(color::background 6)"
-bg_white="$(color::background 7)"
-
-#Brightackground
-bg_bblack="$(color::background 8)"
-bg_bred="$(color::background 9)"
-bg_bgreen="$(color::background 10)"
-bg_byellow="$(color::background 11)"
-bg_bblue="$(color::background 12)"
-bg_bpurple="$(color::background 13)"
-bg_bcyan="$(color::background 14)"
-bg_bwhite="$(color::background 15)"
-
-reset="$(color::reset)"
-bold="$(color::bold)"
-underline="$(color::underline)"
-remove_underline="$(color::nounderline)"
-#blink="$(tput blink)"
-reverse="$(color::reverse)"
-
-error_color="$bwhite$bold"
-warn_color="$white$bold"
+strip_color_codes() {
+    perl -pe 's/\e\[?.*?[\@-~]//g' $content
+}

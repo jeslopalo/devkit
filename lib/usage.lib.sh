d0fa5d129f439ad9ae958106004f3a9123027180
Jesús López Alonso <jeslopalo@sandbox.es>
2018-06-10 22:21:56 +0200

    Add a namespace to 'usage' library

--- lib/usage.lib.sh
+++ lib/usage.lib.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #>|          mark 'synopsis' line
-#+|          mark 'usage' lines
+#+|          mark 'help' lines
 #%|          mark 'purpose' line
 #-|          mark 'authorship' line
-#[%+-=>]|    mark 'man' lines
+#[%+-=>]|    mark 'manual' lines
 
 # inspired by https://stackoverflow.com/a/29579226
-extract_from_header() {
+usage::extract_from_header() {
     local -r pattern="${1:-'#'}"
     local -r source_file="${2:-}"
 
@@ -15,7 +15,7 @@ extract_from_header() {
     grep -e "$pattern" ${source_file} | sed -e "s/$pattern//g" -e "s/^ //" ;
 }
 
-extract_command_main_source() {
+usage::extract_command_main_source() {
     # In "devkit" framework, usage documentation is in "main.sh" script
     # and it's always in the second place of calling stack
     for (( idx=${#BASH_SOURCE[@]}-1 ; idx>=0 ; idx-- )) ; do
@@ -27,24 +27,24 @@ extract_command_main_source() {
     return 1
 }
 
-help() {
-    extract_from_header "^#+|" "${1:-}"
+usage::synopsis() {
+    usage::extract_from_header "^#>|" "${1:-}"
 }
 
-synopsis() {
-    extract_from_header "^#>|" "${1:-}"
+usage::help() {
+    usage::extract_from_header "^#+|" "${1:-}"
 }
 
-purpose() {
-    extract_from_header "^#%|[ ]*" "${1:-}"
+usage::purpose() {
+    usage::extract_from_header "^#%|[ ]*" "${1:-}"
 }
 
-authorship() {
-    extract_from_header "^#-|" "${1:-}"
+usage::authorship() {
+    usage::extract_from_header "^#-|" "${1:-}"
 }
 
-manual() {
-    extract_from_header "^#[%+-=>]|" "${1:-}"
+usage::manual() {
+    usage::extract_from_header "^#[%+-=>]|" "${1:-}"
 }
 
 #
@@ -53,24 +53,24 @@ manual() {
 if [[ $_ != $0 ]]; then
     for option in "$@"; do
         case ${option:-} in
-        --manual)
-            manual "$(extract_command_main_source)"
-            exit 0
-        ;;
-        --purpose)
-            purpose "$(extract_command_main_source)"
-            exit 0
-        ;;
         --synopsis)
-            synopsis "$(extract_command_main_source)"
-            exit 0
-        ;;
-        --authorship)
-            authorship "$(extract_command_main_source)"
+            usage::synopsis "$(usage::extract_command_main_source)"
             exit 0
         ;;
         --help)
-            help "$(extract_command_main_source)"
+            usage::help "$(usage::extract_command_main_source)"
+            exit 0
+        ;;
+        --purpose)
+            usage::purpose "$(usage::extract_command_main_source)"
+            exit 0
+        ;;
+        --authorship)
+            usage::authorship "$(usage::extract_command_main_source)"
+            exit 0
+        ;;
+        --manual)
+            usage::manual "$(usage::extract_command_main_source)"
             exit 0
         ;;
         esac

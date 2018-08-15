7e80103b2d6034342c4886ef3931d9ddc2f77f15
Jesús López Alonso <jeslopalo@sandbox.es>
2018-08-15 14:50:49 +0200

    Removes unused code

--- tests/create-wallet-adjustment.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source ./create-wallet-entry.lib.sh
-
-create_entry "ADJUSTMENT" $1 $2 "$3"
--- tests/user-notificator/test-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./test-wallet-notifications.sh 70
-
-./test-user-modification-notifications.sh 1

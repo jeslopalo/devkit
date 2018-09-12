67cd21de30c3065606666554f9e8775393b025ae
Jesús López Alonso <jeslopalo@sandbox.es>
2018-09-12 16:50:15 +0200

    Fix test indentation

--- test/lib/argument/_init.bash
+++ test/lib/argument/_init.bash
@@ -6,16 +6,16 @@ load_lib bats-support
 load_lib bats-assert
 
 arguments=(\
-"-C" \
-"--color" \
-"--module=devkit" \
-"-m" "ms" \
-"--with-spaces" "this is a message" \
-"-f=1" \
--- \
-"--positional_argument" \
-"will be" \
-ignored \
+    "-C" \
+    "--color" \
+    "--module=devkit" \
+    "-m" "ms" \
+    "--with-spaces" "this is a message" \
+    "-f=1" \
+    -- \
+    "--positional_argument" \
+    "will be" \
+    ignored \
 )
 
 setup() {

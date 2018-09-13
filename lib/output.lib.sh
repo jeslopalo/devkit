d6d16888b51d2142db6ee2d530399d7865020dae
Jesús López Alonso <jeslopalo@sandbox.es>
2018-09-14 00:12:28 +0200

    Improve output::columnize method

--- lib/output.lib.sh
+++ lib/output.lib.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-using column
+using column, xargs, sort, head, read, wc
 
 output::columnize() {
-    local -a elements=$@
+    local -a elements="$@"
+    local -r length=$(__most_longest_element "${elements[@]}")
 
     for value in ${elements[@]}; do
-        printf "%-8s\n" "${value}"
-    done | column -x
+        printf "  %-${length}s\n" "${value}"
+    done | column -x -c "$(__max_width)"
+}
+
+__max_width() {
+    local -r cols="$(tput cols)"
+    local -r max=180
+
+    echo $((cols < max ? cols : max))
+}
+
+__most_longest_element() {
+    echo "$@" | \
+    xargs -n1 -I{} sh -c 'echo $(echo -n {} | wc -c)\\t{}' | \
+    sort -nr | \
+    head -1 | \
+    { read first rest; echo $first; }
 }

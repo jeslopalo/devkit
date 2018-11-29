6a685731da5fab71838965e540f13aebb06a5297
Jesús López Alonso <jeslopalo@sandbox.es>
2018-11-29 23:31:50 +0100

    Refactor 'eureka' command code

--- module/eureka/lib/configuration.lib.sh
+++ module/eureka/lib/configuration.lib.sh
@@ -16,10 +16,14 @@ eureka::environment() {
     config::property --name="environment" --identifier="$eureka_config_identifier"
 }
 
-eureka::find_register_url_pattern() {
-    config::find --filter='.eureka."register-url"' --interpolate --identifier="$eureka_config_identifier"
+eureka::instance_hostname_pattern() {
+    config::find --filter='.eureka."instance-hostname-pattern"' --interpolate --identifier="$eureka_config_identifier"
 }
 
-eureka::find_unregister_url_pattern() {
-    config::find --filter='.eureka."unregister-url"' --interpolate --identifier="$eureka_config_identifier"
+eureka::register_url_pattern() {
+    config::find --filter='.eureka."register-url-pattern"' --interpolate --identifier="$eureka_config_identifier"
+}
+
+eureka::unregister_url_pattern() {
+    config::find --filter='.eureka."unregister-url-pattern"' --interpolate --identifier="$eureka_config_identifier"
 }
